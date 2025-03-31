24da60dba6fc5f3b52d69a71388d4b4ab9da549c
JohnShockley <jshockley@insectria.net>
2025-03-31 16:30:36 -0400

    Corrected player movement and camera follow

--- Assets/Scripts/thirdPersonMovement.cs
+++ Assets/Scripts/thirdPersonMovement.cs
@@ -1,27 +1,88 @@
 using System;
+using Unity.Cinemachine;
 using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class thirdPersonMovement : MonoBehaviour
 {
-
+    #region Movement Variables
+    private Vector2 input;
     public CharacterController controller;
+    private Vector3 moveDirection;
     public float speed = 6f;
-    public float lookSpeed = 1f;
-
     public InputActionReference move;
+    #endregion
+
+    #region Rotation Variables
+    public float rotationSpeed = 1f;
+    #endregion
+    #region Gravity Variables
+    private float gravity = -9.81f;
+    public float gravityMultiplier = 1f;
+    private float downwardVelocity;
+    #endregion
+
+    #region Jump Variables
+    [SerializeField] private float jumpPower;
+    #endregion
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 moveInput = move.action.ReadValue<Vector2>().normalized;
-        Vector3 moveDirection = new Vector3(moveInput.x, 0f, moveInput.y);
-        if (moveDirection.magnitude >= 0.1f)
-        {
-            controller.Move(moveDirection * speed * Time.deltaTime);
-            Quaternion targetRotation = Quaternion.LookRotation(moveDirection);
-            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, lookSpeed);
-        }
+
+        ApplyGravity();
+        ApplyRotation();
+        ApplyMovement();
     }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        input = context.ReadValue<Vector2>();
+        moveDirection = new Vector3(input.x, 0f, input.y);
+    }
+
+    void ApplyRotation()
+    {
+        if (input.sqrMagnitude == 0)
+        {
+            return;
+        }
+        Quaternion targetRotation = Quaternion.LookRotation(new Vector3(input.x, 0f, input.y));
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed);
+    }
+
+    void ApplyMovement()
+    {
+        controller.Move(speed * Time.deltaTime * moveDirection);
+    }
+
+    void ApplyGravity()
+    {
+        if (IsGrounded() && downwardVelocity < 0.0f)
+        {
+            downwardVelocity = -1f;
+        }
+        else
+        {
+            downwardVelocity += gravity * gravityMultiplier * Time.deltaTime;
+        }
+        moveDirection.y = downwardVelocity;
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+
+        if (!context.started)
+        {
+            return;
+        }
+        if (!IsGrounded())
+        {
+            return;
+        }
+        downwardVelocity += jumpPower;
+    }
+
+    private bool IsGrounded() => controller.isGrounded;
 }
